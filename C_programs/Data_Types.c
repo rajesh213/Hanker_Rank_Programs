2d07795909c187559b0104ba5a3a2195d50d2d92
rajesh213 <rajesh07147@gmail.com>
2018-11-11 13:39:29 +0530

    C programs Added

---
+++ C_programs/Data_Types.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    char s[] = "HackerRank ";
+
+
+    int a;
+    double b;
+    char c[105];
+
+
+    printf("enter the a value\n");
+    scanf("%d",&a);
+    printf("enter the b value\n");
+    scanf("%lf",&b);
+    printf("enter the char\n");
+      scanf("%[^'\n']s",c);
+      return 0;
+
+}
